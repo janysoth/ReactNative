0ac0148c8ee748820259db64a97d438b48e70a56
Jonny Vorn Soth <janysoth@gmail.com>
2024-09-27 01:14:36 -0500

    Redirect the first page loaded to the home directory index.js file.

--- employee-management-app/app/(home)/index.js
+++ employee-management-app/app/(home)/index.js
@@ -1,10 +1,11 @@
-import { Redirect } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const index = () => {
   return (
-    <Redirect href="/(home)" />
+    <View>
+      <Text>Home Index</Text>
+    </View>
   );
 };
 
