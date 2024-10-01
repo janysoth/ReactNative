f399be87ca2753adf8922ec4637b77c3689b6acf
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-01 01:38:26 -0500

    Create functions to create and fetch the employees in the backend.

--- employee-management-app/api/index.js
+++ employee-management-app/api/index.js
@@ -22,5 +22,58 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+// Import the models
+const Employee = require("./models/employee");
+const Attendance = require("./models/attendance");
+
+// Endpoint to Register an Employee
+app.post("/addEmployee", async (req, res) => {
+  try {
+    const {
+      employeeName,
+      employeeId,
+      designation,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      activeEmployee,
+      salary,
+      address,
+    } = req.body;
+
+    // Create a new Employee
+    const newEmployee = new Employee({
+      employeeName,
+      employeeId,
+      designation,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      activeEmployee,
+      salary,
+      address,
+    });
+
+    await newEmployee.save();
+
+    res.status(201).json({ message: "Employee saved successfully. ", employee: newEmployee });
+  } catch (error) {
+    console.log("Error in creating an employee. ", error);
+    res.status(500).json({ message: "Failed to add an employee." });
+  }
+});
+
+// Endpoint to Fetch All of the Employees
+app.get("/employees", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (error) {
+    console.log("Error in fetching all of the employees. ", error);
+    res.status(500).json({ message: "Failed to retrieve all of the employees." });
+  }
+})
+
+
 
 
