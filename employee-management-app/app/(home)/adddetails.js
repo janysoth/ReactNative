b88c0089c27659a1005d05ff2958a7339209acaf
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-01 02:14:56 -0500

    Create adddetails screen to add an employee to the database.

---
+++ employee-management-app/app/(home)/adddetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const adddetails = () => {
+  return (
+    <View>
+      <Text>adddetails</Text>
+    </View>
+  );
+};
+
+export default adddetails;
+
+const styles = StyleSheet.create({});
--- employee-management-app/app/(home)/employees.js
+++ employee-management-app/app/(home)/employees.js
@@ -1,10 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import axios from "axios";
+import { useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 const employees = () => {
+  const [employees, setEmployees] = useState([]);
+  const [input, setInput] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchEmployeeData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/employees");
+        setEmployees(response.data);
+      } catch (error) {
+        console.log("Error in fetching employee data. ", error);
+      }
+    };
+    fetchEmployeeData();
+  }, []);
+  console.log(employees);
   return (
-    <View>
-      <Text>employees</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Ionicons
+          onPress={() => router.back()}
+          style={{ marginLeft: 10 }}
+          name="arrow-back"
+          size={24}
+          color="black"
+        />
+
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginHorizontal: 7,
+            gap: 10,
+            backgroundColor: "white",
+            borderRadius: 3,
+            height: 40,
+            flex: 1,
+          }}
+        >
+          <AntDesign
+            style={{ marginLeft: 10 }}
+            name="search1"
+            size={20}
+            color="black"
+          />
+          <TextInput
+            value={input}
+            onChangeText={(text) => setInput(text)}
+            style={{ flex: 1 }}
+            placeholder="Search"
+          />
+
+          {employees.length > 0 && (
+            <View>
+              <Pressable>
+                <AntDesign name="pluscircle" size={30} color="#0072b1" />
+              </Pressable>
+            </View>
+          )}
+        </Pressable>
+      </View>
+
+      <Pressable onPress={() => router.push("/(home)/adddetails")}>
+        <AntDesign name="pluscircle" size={30} color="#0072b1" />
+      </Pressable>
     </View>
   );
 };
