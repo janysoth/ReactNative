646200035f5877e996451284e42b9bcf7e56f7c0
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-08 02:56:08 -0500

    Create markattendance page in the (home) directory.

---
+++ employee-management-app/app/(home)/markattendance.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const markattendance = () => {
+  return (
+    <View>
+      <Text>markattendance</Text>
+    </View>
+  );
+};
+
+export default markattendance;
+
+const styles = StyleSheet.create({});
