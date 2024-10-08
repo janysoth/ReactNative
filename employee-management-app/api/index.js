bb1b4cd2c144634e48a3ad44273356c7f9c8e79d
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-08 02:30:39 -0500

    Create the endpoints for post and fetch the attendance in api folder.

--- employee-management-app/api/index.js
+++ employee-management-app/api/index.js
@@ -88,6 +88,45 @@ app.delete('/employees/:id', async (req, res) => {
   }
 });
 
+app.post("/attendance", async (req, res) => {
+  try {
+    const { employeeId, employeeName, date, status } = req.body;
+
+    const existingAttendance = await Attendance.findOne({ employeeId, date });
+
+    if (existingAttendance) {
+      existingAttendance.status = status;
+
+      await existingAttendance.save();
+      res.status(200).json(existingAttendance);
+    } else {
+      const newAttendance = new Attendance({
+        employeeId,
+        employeeName,
+        date,
+        status,
+      });
+
+      await newAttendance.save();
+      res.status(200).json(newAttendance);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error in submitting attendance." });
+  }
+});
+
+app.get("/attendance", async (req, res) => {
+  try {
+    const { date } = req.query;
+
+    // Find Attendance records for the specified date
+    const attendanceData = await Attendance.find({ date: date });
+
+    res.status(200).json(attendanceData);
+  } catch (error) {
+    res.status(500).json({ message: "Error in fetching attendance data." });
+  }
+});
 
 
 
