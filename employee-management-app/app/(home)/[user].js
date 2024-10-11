92ecda55b0180a6cfaa45ffd3591b2bc60adf612
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-11 05:13:39 -0500

    Finish [user].js page.

--- employee-management-app/app/(home)/[user].js
+++ employee-management-app/app/(home)/[user].js
@@ -54,9 +54,217 @@ const user = () => {
   };
 
   return (
-    <View>
-      <Text>[user]</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginVertical: 20,
+        }}
+      >
+        <AntDesign onPress={goToPrevDay} name="left" size={24} color="black" />
+        <Text>{formatDate(currentDate)}</Text>
+        <AntDesign onPress={goToNextDay} name="right" size={24} color="black" />
+      </View>
+      {/* End of the Date Ticker */}
+
+      <Pressable
+        style={{
+          marginVertical: 10,
+          marginHorizontal: 12,
+          flexDirection: "row",
+          gap: 10,
+        }}
+      >
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 8,
+            padding: 10,
+            backgroundColor: "#4b6cb7",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 16 }}>
+            {params?.name.charAt(0)}
+          </Text>
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+            {params?.name}
+          </Text>
+          <Text style={{ marginTop: 5, color: "gray" }}>
+            {params?.designation} ({params?.id})
+          </Text>
+        </View>
+      </Pressable>
+      {/* End of the Employee Pressable */}
+
+      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 12 }}>
+        Basic Pay : {params?.salary}
+      </Text>
+
+      <View style={{ marginHorizontal: 12 }}>
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "500",
+            letterSpacing: 3,
+            marginTop: 7,
+          }}
+        >
+          ATTENDANCE
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 16,
+            marginVertical: 10,
+          }}
+        >
+          <Pressable
+            onPress={() => setAttendanceStatus("present")}
+            style={{
+              backgroundColor: "#C4E0E5",
+              padding: 10,
+              borderRadius: 8,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flex: 1,
+            }}
+          >
+            {attendanceStatus === "present" ? (
+              <FontAwesome5 name="dot-circle" size={24} color="black" />
+            ) : (
+              <Entypo name="circle" size={24} color="black" />
+            )}
+            <Text>Present</Text>
+          </Pressable>
+
+          <Pressable
+            onPress={() => setAttendanceStatus("absent")}
+            style={{
+              backgroundColor: "#C4E0E5",
+              padding: 10,
+              borderRadius: 8,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flex: 1,
+            }}
+          >
+            {attendanceStatus === "absent" ? (
+              <FontAwesome5 name="dot-circle" size={24} color="black" />
+            ) : (
+              <Entypo name="circle" size={24} color="black" />
+            )}
+            <Text>Absent</Text>
+          </Pressable>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 16,
+            marginVertical: 10,
+          }}
+        >
+          <Pressable
+            onPress={() => setAttendanceStatus("halfday")}
+            style={{
+              backgroundColor: "#C4E0E5",
+              padding: 10,
+              borderRadius: 8,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flex: 1,
+            }}
+          >
+            {attendanceStatus === "halfday" ? (
+              <FontAwesome5 name="dot-circle" size={24} color="black" />
+            ) : (
+              <Entypo name="circle" size={24} color="black" />
+            )}
+            <Text>Half Day</Text>
+          </Pressable>
+
+          <Pressable
+            onPress={() => setAttendanceStatus("holiday")}
+            style={{
+              backgroundColor: "#C4E0E5",
+              padding: 10,
+              borderRadius: 8,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flex: 1,
+            }}
+          >
+            {attendanceStatus === "holiday" ? (
+              <FontAwesome5 name="dot-circle" size={24} color="black" />
+            ) : (
+              <Entypo name="circle" size={24} color="black" />
+            )}
+            <Text>Holiday</Text>
+          </Pressable>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+          <TextInput
+            style={{
+              borderRadius: 6,
+              marginTop: 10,
+              borderWidth: 2,
+              borderColor: "#E0E0E0",
+              padding: 10,
+              flex: 1,
+            }}
+            placeholderTextColor="black"
+            placeholder="Advance / Loans"
+          />
+          <TextInput
+            style={{
+              borderRadius: 6,
+              marginTop: 10,
+              borderWidth: 2,
+              borderColor: "#E0E0E0",
+              padding: 10,
+              flex: 1,
+            }}
+            placeholderTextColor="black"
+            placeholder="Extra Bonus"
+          />
+        </View>
+
+        <Pressable
+          onPress={submitAttendance}
+          style={{
+            padding: 15,
+            backgroundColor: "#00c6ff",
+            width: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 30,
+            borderRadius: 6,
+          }}
+        >
+          <Text
+            style={{ textAlign: "center", color: "white", fontWeight: "500" }}
+          >
+            Submit Attendance
+          </Text>
+        </Pressable>
+      </View>
     </View>
+    // End of the Main View
   );
 };
 
